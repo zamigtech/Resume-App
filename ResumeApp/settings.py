17e198f326b4d82f3639ec4dc42d5284786e1096
zamigtech <zamigtech@gmail.com>
2024-02-08 17:36:27 +0400

    feat: enable continuous development

--- ResumeApp/settings.py
+++ ResumeApp/settings.py
@@ -17,6 +17,8 @@ import environ
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
