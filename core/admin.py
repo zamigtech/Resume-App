63d7dabb8e45e1d66a16461225d6cc2f6d541be2
zamigtech <zamigtech@gmail.com>
2024-04-06 19:28:14 +0400

    feat: add general setting model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import *
 
 # Register your models here.
+
+@admin.register(GeneralSetting)
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'parameters', 'updated_date', 'created_date']
+    search_fields = ['name', 'description', 'parameters']
+    list_editable = ['description', 'parameters']
+
+    class Meta:
+        model = GeneralSetting
+
