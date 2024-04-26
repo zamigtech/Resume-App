e89bcdf31bad0f5be32443b24732b032bac8301a
zamigtech <zamigtech@gmail.com>
2024-04-26 07:29:59 +0400

    feat: create message model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from core.models import AbstractModel
+
 
 # Create your models here.
+
+class Message(AbstractModel):
+    name = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name="Name",
+        help_text="",
+    )
+    email = models.EmailField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name="Email",
+        help_text="",
+    )
+    subject = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name="Subject",
+        help_text="",
+    )
+    message = models.TextField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name="Message",
+        help_text="",
+    )
+
+    def __str__(self):
+        return f'Message: {self.name}'
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Message'
+        ordering = ('name',)
+
--- contact/views.py
+++ contact/views.py
@@ -2,16 +2,40 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 
+from contact.models import Message
+
 
 
 
 # Create your views here.
 
 def contact_form(request):
-    context = {'success': True,
-               'message': 'Contact form sent successfully!'
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        Message.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+        )
+        success = True
+        message = "Your message has been sent successfully"
+    else:
+        success = False
+        message = 'Request method is not a valid.'
+
+    context = {
+        'success': success,
+        'message': message,
     }
     return JsonResponse(context)
+
+
+
 
 
 def contact(request):
